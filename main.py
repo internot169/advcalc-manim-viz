95ba75b6e4dd95a98c814bae61168069e13e9955
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2024-04-20 14:17:31 -0700

    mas code

--- main.py
+++ main.py
@@ -4,22 +4,28 @@ from math import sin
 
 class GraphExample(ThreeDScene):
     def construct(self):
-        curve1 = ParametricFunction(
+        self.set_camera_orientation(phi=75 * DEGREES, theta=-15 * DEGREES)
+        
+        curve = ParametricFunction(
             lambda u: np.array([
                 1+u,
                 3*u,
                 4-(5*u)
             ]), color = RED, t_range=np.array([-1 * TAU, 1 * TAU, 0.001])
         ).set_shade_in_3d(True)
+        
         axes = ThreeDAxes()
-        dot_1 = Dot3D(point=axes.coords_to_point(1, 0, 4), color=RED)
-        dot_2 = Dot3D(point=axes.coords_to_point(2, 3, -1), radius=0.1, color=BLUE)
-        dot_3 = Dot3D(point=[0, 0, 0], radius=0.1, color=ORANGE)
-        self.set_camera_orientation(phi=75 * DEGREES, theta=-15 * DEGREES)
         labels = axes.get_axis_labels(x_label="x", y_label="y", z_label="z")
-        # self.begin_ambient_camera_rotation(rate=0.1)
+        
+        dot_1 = Dot3D(point=axes.coords_to_point(1, 0, 4), radius=0.1, color=RED)
+        dot_2 = Dot3D(point=axes.coords_to_point(2, 3, -1), radius=0.1, color=BLUE)
+        dot_3 = Dot3D(point=[0, 1, -2], radius=0.1, color=ORANGE)
+        self.add(dot_1, dot_2, dot_3)
+        
+        
+        self.begin_ambient_camera_rotation(rate=0.1)
         self.play(Write(axes), Write(labels))
-        self.add(axes, dot_1, dot_2, dot_3)
+        self.play(Create(curve))
 
         # line = axes.plot_parametric_curve(lambda u: np.array([
         #         1+u,
@@ -31,6 +37,6 @@ class GraphExample(ThreeDScene):
         
         self.wait(4.0)
 
-        self.play(Unwrite(axes), Unwrite(labels), Uncreate(curve1))
+        self.play(Unwrite(axes), Unwrite(labels), Uncreate(curve))
         # self.begin_ambient_camera_rotation(rate=0.1)
         self.wait()
